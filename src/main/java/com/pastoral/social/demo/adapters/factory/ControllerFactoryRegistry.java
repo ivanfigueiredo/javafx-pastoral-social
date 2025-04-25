e893a2682ae2b7ce231dc57a0bcc585f33d0fd94
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2025-04-24 22:48:10 -0300

    feat: criando container de dependencias para utilizar com JavaFX

---
+++ src/main/java/com/pastoral/social/demo/adapters/factory/ControllerFactoryRegistry.java
@@ -0,0 +1,35 @@
+package com.pastoral.social.demo.adapters.factory;
+
+import com.pastoral.social.demo.adapters.controller.CadastroAlimentoController;
+import com.pastoral.social.demo.application.exceptions.InternalServerErrorException;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Consumer;
+
+public final class ControllerFactoryRegistry {
+    private static final Map<Class<?>, Consumer<?>> registry = new HashMap<>();
+
+    static {
+        registry.put(CadastroAlimentoController.class, (CadastroAlimentoController cadastroAlimentoController) -> {
+            cadastroAlimentoController.setAdicionarAlimentoUseCase(ResolveDependencyFactory.createAdicionarAlimentoUseCase());
+            cadastroAlimentoController.setListUnidadeDeMedidasUseCase(ResolveDependencyFactory.createListUnidadeDeMedidasUseCase());
+            cadastroAlimentoController.setListCategoriaUseCase(ResolveDependencyFactory.createListCategoriaUseCase());
+            cadastroAlimentoController.setListLocalizacaoUseCase(ResolveDependencyFactory.createListLocalizacaoUseCase());
+            cadastroAlimentoController.setup();
+        });
+    }
+
+    private ControllerFactoryRegistry() {}
+
+    @SuppressWarnings("unchecked")
+    public static <T> void inject(T controller) {
+        final Consumer<T> consumer = (Consumer<T>) registry.get(controller.getClass());
+        if (Objects.nonNull(consumer)) {
+            consumer.accept(controller);
+        } else {
+            throw new InternalServerErrorException("Nenhum registro encontrado para o controller " + controller.getClass().getName());
+        }
+    }
+}
