8f44bfb15a3897f3eb38a67a30c360b11ce4512f
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2025-05-13 21:45:55 -0300

    refactor: adicionando tratamento mais fino das permissoes

--- src/adapters/http/HttpClient.ts
+++ src/adapters/http/HttpClient.ts
@@ -1,11 +1,13 @@
 import { NextFunction, Request, Response } from "express";
 
+export type CallbackFunction = (req: Request, res: Response, next: NextFunction) => void;
+
 export interface HttpClient {
     on: (
         method: string, 
         url: string, 
-        middlewareAuth: (req: Request, res: Response, next: NextFunction) => void, 
-        middlewareAuthorize: (req: Request, res: Response, next: NextFunction) => void, 
+        middlewareAuth: CallbackFunction,
+        middlewareAuthorize: CallbackFunction, 
         callback: (params: any, data: any) => any
     ) => void;
     listen: (port: number, callback: Function) => void;
--- src/adapters/http/authentication/Auth.ts
+++ src/adapters/http/authentication/Auth.ts
@@ -1,12 +1,19 @@
 import { NextFunction, Request, Response } from "express";
+import { randomUUID } from "node:crypto";
 
 export class Auth {
     private auth = "secreto";
     
     public async authentication(req: Request, res: Response, next: NextFunction): Promise<void> {
         const token = req.headers.authorization;
+        const role = req.headers['role-x-user'] as string;
         if (token && token === this.auth) {
+            req.user = {
+                id: randomUUID(),
+                role
+            }
             next();
+
         } else {
             res.status(401).json({ error: 'Unauthorized' });
         }
