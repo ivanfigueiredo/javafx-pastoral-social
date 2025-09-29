b517142eb475dee23775dc324da0efc66035b1e8
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2025-09-28 22:18:23 -0300

    feat: criando entrypoint Rest para o GET de listagem de templates

--- src/adapters/controller/TemplateController.ts
+++ src/adapters/controller/TemplateController.ts
@@ -1,11 +1,29 @@
+import { NextFunction, Response, Request } from "express";
 import { TemplateUseCase } from "../../application/port/in/TemplateUseCase";
 import { HttpClient } from "../http/HttpClient";
+import { UserLogged } from "../http/types/express";
+import { GetTemplatesDTO } from "../../application/dto/GetTemplatesDTO";
 
 export class TemplateController {
     constructor(
         readonly httpClient: HttpClient,
         readonly templateUseCase: TemplateUseCase
     ) {
-        
+        httpClient.on(
+            "get", 
+            "/template/lista", 
+            (req: Request, res: Response, next: NextFunction) => next(),
+            (req: Request, res: Response, next: NextFunction) => next(),
+            // auth.authentication.bind(auth),
+            // async (req: Request, res: Response, next: NextFunction) => authorize.can(req, res, next, ActionType.ListarDificuldade),
+            async function (_params: any, _data: any, _userLogged?: UserLogged, query?: GetTemplatesDTO) {
+                const output = await templateUseCase.listarTemplates(query!);
+                return {
+                    statusCode: 200,
+                    timeStampe: new Date().toISOString(),
+                    data: output
+                };
+            }
+        );
     }
 }
