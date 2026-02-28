7ddc8691d7a00807928e75905acf78a523a01071
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2026-02-28 17:02:48 -0300

    chore: arquivo estatico para deploy na vercel

--- src/adapters/logger/logger.ts
+++ src/adapters/logger/logger.ts
@@ -1,6 +1,7 @@
 import pino from 'pino';
 
-export const loggerAdapter = pino({
-    level: 'info',
-    redact: ["req.headers.authorization", "req.body.senha"],
-});
+export const loggerAdapter = pino(
+    {
+        level: 'info',
+        redact: ["req.headers.authorization", "req.body.senha"],
+    }, pino.destination({ sync: true }));
